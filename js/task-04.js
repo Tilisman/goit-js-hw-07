5df42270fdb5f2d343402d5abe9e0c881900da74
Anthony Tilis <tilissound@gmail.com>
2021-06-03 02:18:25 +0300

    task-4.1

--- js/task-04.js
+++ js/task-04.js
@@ -1,18 +1,18 @@
 let counterValue = 0;
 
-const increment = () => {
+const btnIncrClickHandler = () => {
 	counterValue += 1;
 	return counterEl.textContent = counterValue};
 
-const decrement = () => {
+const btnDecrClickHandler = () => {
 	counterValue -= 1;
 	return counterEl.textContent = counterValue};
 
 const counterEl = document.querySelector('#value');
 const btnIncr = document.querySelector('[data-action="increment"]');
 const btnDecr = document.querySelector('[data-action="decrement"]');
-btnIncr.addEventListener('click', increment);
 
-btnDecr.addEventListener('click', decrement);
+btnIncr.addEventListener('click', btnIncrClickHandler);
+btnDecr.addEventListener('click', btnDecrClickHandler);
 
 

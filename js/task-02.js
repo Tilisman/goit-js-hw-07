199611021ac46ac85f1ed58c65d5aabd3b704c29
Anthony Tilis <tilissound@gmail.com>
2021-06-02 23:42:29 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const ingredientsRef = document.querySelector('#ingredients');
+
+const ingrCollection = ingredients.map(ingr => {
+const ingredientEl = document.createElement('li')
+ingredientEl.textContent = ingr
+return ingredientEl
+});
+
+ingredientsRef.append(...ingrCollection);
+
+
+

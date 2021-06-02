eb70b1cdcd28d1a0d965d0d15bd9a31c6d236666
Anthony Tilis <tilissound@gmail.com>
2021-06-02 22:53:06 +0300

    task1.1

--- js/task-01.js
+++ js/task-01.js
@@ -6,7 +6,7 @@ console.log(`В списке ${catRef.children.length} категории.`);
 
 const itemRef = document.querySelectorAll('.item');
 
-itemRef.forEach(item => console.log(`Категория: ${item.querySelector('h2').textContent},
+itemRef.forEach(item => console.table(`Категория: ${item.querySelector('h2').textContent}
 Количество элементов: ${item.querySelectorAll('li').length}
 `));
 
